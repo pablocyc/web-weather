200545d82e92f065e5f05bcc5d65aa491a0d03a4
Pablo Cardozo <pablocyc83@gmail.com>
2022-03-04 01:03:07 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,19 +1,20 @@
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = "offline";
+// eslint-disable-next-line
+const OFFLINE_VERSION = 1
+const CACHE_NAME = "offline"
 // Modifica esto con una diferente URL si es necesario.
-const OFFLINE_URL = "offline.html";
+const OFFLINE_URL = "offline.html"
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      // Al definir {cache: 'reload'} en la nueva consulta asegurara que la respuesta no sea desde el caché de HTTP; i.e., esta será de la red.
-      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+      const cache = await caches.open(CACHE_NAME)
+      // Al definir {cache: "reload"} en la nueva consulta asegurara que la respuesta no sea desde el caché de HTTP; i.e., esta será de la red.
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }))
     })()
-  );
+  )
   // Obliga al service worker que espera a que se convierta en uno activo.
-  self.skipWaiting();
-});
+  self.skipWaiting()
+})
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
@@ -21,14 +22,14 @@ self.addEventListener("activate", (event) => {
       // Permite la navegación precargada si tiene compatibilidad
       // Mira https://developers.google.com/web/updates/2017/02/navigation-preload
       if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
+        await self.registration.navigationPreload.enable()
       }
     })()
-  );
+  )
 
   // Le dice al service worker activo que tome el control inmediato de la página.
-  self.clients.claim();
-});
+  self.clients.claim()
+})
 
 self.addEventListener("fetch", (event) => {
   // Solo queremos llamar al event.respondWith() si es una solicitud de navegación
@@ -38,53 +39,53 @@ self.addEventListener("fetch", (event) => {
       (async () => {
         try {
           // Primero, utiliza una respuesta de precarga de navegación.
-          const preloadResponse = await event.preloadResponse;
+          const preloadResponse = await event.preloadResponse
           if (preloadResponse) {
-            return preloadResponse;
+            return preloadResponse
           }
 
           // Siempre usa la red primero.
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
+          const networkResponse = await fetch(event.request)
+          return networkResponse
         } catch (error) {
           // El catch solo se dispara cuando se obtiene una excepción gracias a un error en la red.
           // Si fetch() regresa una respuesta HTTP valida con un codigo de respuesta en el rango de 4xx o 5xx, el catch() no se llamará
-          console.log("Fetch failed; returning offline page instead.", error);
+          console.log("Fetch failed; returning offline page instead.", error)
 
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
+          const cache = await caches.open(CACHE_NAME)
+          const cachedResponse = await cache.match(OFFLINE_URL)
+          return cachedResponse
         }
       })()
-    );
+    )
   }
 
-  // si nuestra condición de if() es falso, el controlador de fetch no atrapará la solicitud. Si hay más controladores de fetch registrados, ellos tendrán la oportunidad de llamar a event.respondWith(). 
+  // si nuestra condición de if() es falso, el controlador de fetch no atrapará la solicitud. Si hay más controladores de fetch registrados, ellos tendrán la oportunidad de llamar a event.respondWith().
   // De lo contrario, si no hay, no se llamará a event.respondWith(), la solicitud será controlada por el buscador como si no los service worker no se hubieran involucrado.
-});
-
-self.addEventListener('push', e => {
-  const data = e.data.json();
-  console.log('push', data);
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: './assets/cloud-night.png'
-  });
 })
 
-self.addEventListener('notificationclick', e => {
+self.addEventListener("push", e => {
+  const data = e.data.json()
+  console.log("push", data)
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "./assets/cloud-night.png"
+  })
+})
+
+self.addEventListener("notificationclick", e => {
   const notification = e.notification
   const action = e.action
 
-  if (action === 'accept') {
-    console.log('Notification accepted')
+  if (action === "accept") {
+    console.log("Notification accepted")
     notification.close()
   } else {
-    console.log('Notification dismissed')
+    console.log("Notification dismissed")
     notification.close()
   }
 })
 
-self.addEventListener('notificationclose', e => {
-  console.log('Notification was closed', e)
+self.addEventListener("notificationclose", e => {
+  console.log("Notification was closed", e)
 })
